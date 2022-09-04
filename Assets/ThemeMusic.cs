f6e9035e88774e908c4dbd45e2c7fd3efb345bac
jdim21 <kd218@protonmail.com>
2022-09-03 20:29:50 -0400

    theme music spcyoseph

---
+++ Assets/ThemeMusic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeMusic : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private static ThemeMusic instance = null;
+    public static ThemeMusic Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
